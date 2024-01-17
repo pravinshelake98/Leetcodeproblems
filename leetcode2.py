b81a3deaccd3c7afae5297bcd46099219ade1e6d
pravin shelake <pravinshelake1997@gmail.com>
2024-01-17 21:01:39 +0530

    merge two list

---
+++ leetcode2.py
@@ -0,0 +1,16 @@
+#You are given the heads of two sorted linked lists list1 and list2.
+
+#Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+#Return the head of the merged linked list.
+
+l1 = [1,2,3,4]
+l2 = [2,3,45,5,6,7]
+
+def merge_twolist(n1,n2):
+    res = sorted(l1+l2)
+    return res
+return []
+merge_twolist(l1,l2)
+
+
